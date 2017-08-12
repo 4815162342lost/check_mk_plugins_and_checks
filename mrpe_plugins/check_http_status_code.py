2f87f425f9f91eb1f001d041f3232d63266c72fe
Ilyas <family-gan@yandex.ru>
2017-08-12 13:49:04 +0300

    Update check_http_status_code.py

--- mrpe_plugins/check_http_status_code.py
+++ mrpe_plugins/check_http_status_code.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 '''Simple script for check http status code.
 exit codes: 0 -- al OK (httpd status code = 200), 2 -- error (connection troubles or http status code not 200)
-mrpe.cfg example: 
-yandex_check /etc/check-mk-agent/check_http_status_code.py  "http://yandex.ru"
-Have fun!'''
+using: check_http_status_code.py site timeout_time
+mrpe.cfg example:
+yandex_check /etc/check-mk-agent/check_http_status_code.py "http://yandex.ru" 10'''
 
 import requests
 import os
 import sys
 
 web_site=sys.argv[1]
+timeout=float(sys.argv[2])
+
 try:
-    r=requests.get(web_site)
+    r=requests.get(web_site, timeout=timeout)
 except:
     print("Critical error: connection troubles")
     exit(2)
 
-if r.status_code == 200:
+if r.status_code == requests.codes.ok:
     print("OK: all OK. Http status code: 200")
     exit(0)
 else:
     print("Critical error! Http status code: {status_code}".format(status_code = r.status_code))
-    exit(2)
+exit(2)
